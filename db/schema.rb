764f56e6814a3f35ba4650b78f395454b23cd642
Nicole W <nicolemairead4@gmail.com>
2017-11-28 12:15:03 -0500

    add jobs and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128162056) do
+ActiveRecord::Schema.define(version: 20171128170621) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.text "description"
+    t.string "origin"
+    t.string "destination"
+    t.integer "cost"
+    t.integer "containers"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
