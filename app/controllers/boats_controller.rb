34f116664e160d9367a7d9ea3cf773c539353ba6
Nicole W <nicolemairead4@gmail.com>
2017-11-28 13:44:49 -0500

    boats controller and views

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -2,17 +2,42 @@ class BoatsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  end
-
-  def new
-  end
-
-  def edit
+    @boats = Boat.all
   end
 
   def show
+    find_params
   end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    find_params
+  end
+
 
   def destroy
   end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :location, :containers)
+  end
+
+  def find_params
+    @boat = Boat.find_by(params[:name])
+  end
+
 end
