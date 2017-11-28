8c67276abcc04b3ad47df98a596aba155a483b66
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2017-11-28 12:23:19 -0500

    Add boats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128162056) do
+ActiveRecord::Schema.define(version: 20171128172126) do
+
+  create_table "boats", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "containers"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
