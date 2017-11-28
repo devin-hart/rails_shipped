d811cf97a119e6db72bfa9e81c7a1ccd60be555f
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2017-11-28 13:45:32 -0500

    jobs stuff

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,14 +2,43 @@ class JobsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @jobs = Jobs.all
   end
 
   def show
+    @job = Job.find(params[:id])
   end
 
   def edit
+    find_params
   end
 
   def new
+    @job = Jobs.new
+  end
+
+  def create
+    @job = current_user.jobs.new(job_params)
+    if @job.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    find_params
+    @job.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:description, :origin, :destination, :cost, :containers )
+  end
+
+  def find_params
+    @job = Job.find(params[:id])
   end
 end
